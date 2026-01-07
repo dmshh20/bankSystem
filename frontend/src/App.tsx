02dbdb8d5da5af973fceac97aa9a7968afdd4c3b
Artem Dmysh <dmshh@gmail.com>
2026-01-07 22:00:57 +0200

    feat: implementation home components as total expense, total finance, recent transactions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from './Header/Header'
+import Home from './Home/Home'
 
 const App = () => {
   return (
     <>
+    <div className='app'>
+
       <Header></Header>
+      <Home></Home>
+    </div>
     </>
   )
 }
